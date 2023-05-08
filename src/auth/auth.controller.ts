f479995555848af5bc6db057f189cb3ac75a4edf
Andrey Peshko <coursestud@mail.ru>
2023-05-08 16:59:25 +0300

    feat: add guard route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './utils/guards/auth.guard';
+import { Request as ReqExpress } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,12 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signDto: Record<string, string>) {
     return this.authService.signIn(signDto.username, signDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: ReqExpress) {
+    //@ts-ignore
+    return req.user;
   }
 }
