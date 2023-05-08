30f9d9378c694025ae52a2aa51a5de11182db9cb
Andrey Peshko <coursestud@mail.ru>
2023-05-08 16:37:23 +0300

    feat: add sign token

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signDto: Record<string, string>) {
+    return this.authService.signIn(signDto.username, signDto.password);
+  }
+}
