15496dffa6e419f17fc5dd49ac8b351ea98102b0
Andrey Peshko <coursestud@mail.ru>
2023-05-08 15:09:34 +0300

    feat: test cookie

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { GetCookie } from 'src/utils/decorators/cookies.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService){}
   @Get()
-  getUser() {
+  getUser(@GetCookie('name') name: string) {
+    console.log(name);
     return `user`;
   }
 }
